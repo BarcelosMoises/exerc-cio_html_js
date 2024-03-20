426301019809917a311857c456494a0335a6f391
Moises Barcelos <barcelos_moises@yahoo.com>
2024-03-19 22:28:56 -0300

    Validação dos números

--- main.js
+++ main.js
@@ -4,15 +4,26 @@ function validaNumero(){
     var nA = document.getElementById('n1').value;
     var nB = document.getElementById('n2').value;
     
-    if ((nA) > (nB)){
-        return true;
-    }   else{
-        alert('O primeiro número deve ser maior que o segundo');
-        return false;
+    if ((nA) < (nB)){
+        const mensagemSucesso = `Formulário preenchido corretamente. O número <b>${nB}</b> é maior que o número <b>${nA}</b>`;
+        const containerMensagemSucesso = document.querySelector('.sucess-message');
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'block';
+
+        var nA = document.getElementById('n1').value='';
+        var nB = document.getElementById('n2').value='';
+    }   else {
+        const mensagemErro = `O número <b>${nA}</b> é maior ou igual ao número <b>${nB}</b>`;
+        const containerMensagemErro = document.querySelector('.error-message');
+        containerMensagemErro.innerHTML = mensagemErro;
+        containerMensagemErro.style.display = 'block';
+
+        var nA = document.getElementById('n1').value='';
+        var nB = document.getElementById('n2').value='';
     }
 }
 
-form.addEventListener('submit', validaNumero, function(e) {
+form.addEventListener('submit',function(e) {
     e.preventDefault();
+    validaNumero();
 })
-
